abd2ba15cfe19acb9b332be9c912880ef60491fa
karlo-kasi <karlokasi1995@gmail.com>
2025-01-28 18:32:20 +0100

    Add: generate new emails

--- main.js
+++ main.js
@@ -3,27 +3,37 @@
 
 
 let list = document.getElementById("list")
+let btn = document.getElementById ("btn")
 
+function generaEmail (){
+    for (i = 0; i < 10; i++) {
 
-
-for (i = 0; i < 10; i++) {
-
-    fetch("https://flynn.boolean.careers/exercises/api/random/mail", { method: "GET" })
-        .then(response => response.json())
-        .then(data => {
-            
-            console.log(data.response)
-            list.innerHTML +=
+        fetch("https://flynn.boolean.careers/exercises/api/random/mail", { method: "GET" })
+            .then(response => response.json())
+            .then(data => {
+                
+                console.log(data.response)
+                list.innerHTML +=
                 `
-            <li>
-            ${data.response}                
-            </li>
-            `
-        })
-        .catch (error => {
-            console.error(error)
-        })  
+                <li>
+                ${data.response}                
+                </li>
+                `
+            })
+            .catch (error => {
+                console.error(error)
+            })  
+    }
 }
+
+generaEmail ()
+
+btn.addEventListener ('click', function (){
+    list.innerHTML = ""
+    generaEmail ()
+})
+
+
 
 
 
